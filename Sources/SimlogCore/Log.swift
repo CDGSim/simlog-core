b70ee42fec8d0564e27e37642097977b3286f6ef
Axel Péju <axel.peju@aviation-civile.gouv.fr>
2021-01-22 20:01:58 +0100

    Add missing public initializers

--- Sources/SimlogCore/Log.swift
+++ Sources/SimlogCore/Log.swift
@@ -17,12 +17,31 @@ public struct Log: Codable {
         enum CodingKeys: String, CodingKey {
                 case name, updateDate = "update_date", configuration, trafficDensity = "traffic_density", objectives, description, startDate = "start_date", duration, pressure, weather
         }
+        
+        public init(name: String, updateDate: Date, configuration: String, trafficDensity: String, objectives: String, description: String, startDate: Date, duration: Int, pressure: Int, weather: String) {
+            self.name = name
+            self.updateDate = updateDate
+            self.configuration = configuration
+            self.trafficDensity = trafficDensity
+            self.objectives = objectives
+            self.description = description
+            self.startDate = startDate
+            self.duration = duration
+            self.pressure = pressure
+            self.weather = weather
+        }
     }
     
     public struct Event: Codable {
         public var time: String
         public var callsign: String
         public var description: String
+        
+        public init(time: String, callsign: String, description: String) {
+            self.time = time
+            self.callsign = callsign
+            self.description = description
+        }
     }
     
     public struct InstructorLog: Codable {
@@ -32,10 +51,14 @@ public struct Log: Codable {
         enum CodingKeys: String, CodingKey {
                 case setupInfo = "setup_info", events
         }
+        
+        internal init(setupInfo: String? = nil, events: [Log.Event]? = nil) {
+            self.setupInfo = setupInfo
+            self.events = events
+        }
     }
     
     public struct PilotLog: Codable {
-        
         public struct SetupElement: Codable {
             public var callsign: String
             public var description: String
@@ -46,6 +69,14 @@ public struct Log: Codable {
         public var directives: String?
         public var setup: [SetupElement]?
         public var events: [Event]?
+        
+        public init(role: String, frequency: String? = nil, directives: String? = nil, setup: [Log.PilotLog.SetupElement]? = nil, events: [Log.Event]? = nil) {
+            self.role = role
+            self.frequency = frequency
+            self.directives = directives
+            self.setup = setup
+            self.events = events
+        }
     }
     
     public var properties: Properties
