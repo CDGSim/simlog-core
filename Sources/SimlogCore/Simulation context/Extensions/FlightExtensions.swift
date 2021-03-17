5025664623aab3890a674a77125663ec04dd6749
Axel Péju <axel.peju@aviation-civile.gouv.fr>
2021-03-17 10:37:49 +0100

    Add estimated IAF time

--- Sources/SimlogCore/Simulation context/Extensions/FlightExtensions.swift
+++ Sources/SimlogCore/Simulation context/Extensions/FlightExtensions.swift
@@ -11,4 +11,11 @@ public extension Flight {
     func estimatedMovementTime() -> Date? {
         return SimulationContext.shared?.estimatedDateToRunway(for: self)
     }
+    
+    func estimatedIAFTime() -> Date? {
+        guard let iaf = self.route.last?.fix else {
+            return nil
+        }
+        return SimulationContext.shared?.estimatedDate(at: iaf, for: self)
+    }
 }
